ead85b1b7d6d963f113bb259ec1380d6d048979f
Fernando Daciuk <f.daciuk@gmail.com>
2015-02-28 19:28:08 +0000

    Contact form finished

--- functions/services/service-contact-form.php
+++ functions/services/service-contact-form.php
@@ -3,9 +3,128 @@ require_once __DIR__ . '/../security.php';
 
 class Service_Contact_Form {
     public function send_contact_form() {
-        return (object) array(
-            'status' => 'success',
-            'message' => 'Seu e-mail foi enviado com sucesso!'
+        if( !isset( $_POST[ 'field_name' ] ) ) {
+            return false;
+        }
+
+        $all_fields = $this->get_fields();
+        $are_all_fields_ok = $this->are_all_fields_ok( $all_fields );
+
+        if( ! $are_all_fields_ok ) {
+            return $this->get_status_message( 'error' );
+        }
+
+        if( $this->is_spam() ) {
+            return $this->get_status_message( 'error_sent' );
+        }
+
+        if( ! $this->message_sent( $all_fields ) ) {
+            return $this->get_status_message( 'error_sent' );
+        }
+
+        return $this->get_status_message( 'success' );
+    }
+
+    private function get_fields() {
+        $fields = array(
+            'name' => array(
+                'value' => sanitize_text_field( $_POST[ 'field_name' ] ),
+                'is_required' => true
+            ),
+            'email' => array(
+                'value' => sanitize_email( $_POST[ 'field_email' ] ),
+                'is_required' => true
+            ),
+            'subject' => array(
+                'value' => sanitize_text_field( $_POST[ 'field_subject' ] ),
+                'is_required' => false
+            ),
+            'message' => array(
+                'value' => wp_kses( $_POST[ 'field_message' ], 'br' ),
+                'is_required' => true
+            )
         );
+
+        return $fields;
+    }
+
+    private function are_all_fields_ok( $fields ) {
+        foreach( $fields as $field ) {
+            if( $this->is_field_required_empty( $field ) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function is_field_required_empty( $field ) {
+        return $field[ 'is_required' ] && empty( $field[ 'value' ] );
+    }
+
+    private function is_spam() {
+        $has_malicious_content = preg_match( '/bcc:|cc:|multipart|\[url|\[link|Content-Type:/i', implode( $_POST ) );
+        $has_more_than_three_links = preg_match_all( '/<a|http:/i', implode( $_POST ) ) > 3;
+        $spam_field_is_filled = ! empty( $_POST[ 'field_mail' ] );
+
+        if( $has_malicious_content ||
+            $has_more_than_three_links ||
+            $spam_field_is_filled
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function get_status_message( $status = 'success' ) {
+        $message = array(
+            'success' => $this->get_status_message_success(),
+            'error' => $this->get_status_message_error(),
+            'error_sent' => $this->get_status_message_error_sent()
+        );
+        return (object) $message[ $status ];
+    }
+
+    private function get_status_message_success() {
+        return array(
+            'message' => 'Mensagem enviada com sucesso!',
+            'status' => 'success'
+        );
+    }
+
+    private function get_status_message_error() {
+        return array(
+            'message' => 'Preencha todos os campos!',
+            'status' => 'error'
+        );
+    }
+
+    private function get_status_message_error_sent() {
+        return array(
+            'message' => 'Erro ao enviar mensagem. Tente novamente mais tarde.',
+            'status' => 'error'
+        );
+    }
+
+    private function message_sent( $fields ) {
+        $to = get_post_meta( get_the_ID(), 'contact_email', true );
+        $subject = '[MCD] E-mail do site - ' . $fields[ 'subject' ][ 'value' ];
+        $message = $this->message_to_sent( $fields );
+        $headers = array(
+            // Cópia oculta pra mim :)
+            'Bcc: f.daciuk+mcd@gmail.com',
+            'Reply-To: ' . $fields[ 'email' ][ 'value' ]
+        );
+        $attachments = '';
+
+        return wp_mail( $to, $subject, $message, $headers, $attachments );
+    }
+
+    private function message_to_sent( $fields ) {
+        $message  = sprintf( '<h1>Assunto: %s</h1>', $fields[ 'subject' ][ 'value' ] );
+        $message .= sprintf( '<h2>De: %s</h2>', $fields[ 'email' ][ 'value' ] );
+        $message .= sprintf( '<h2>Mensagem:</h2><p>%s</p>', $fields[ 'message' ][ 'value' ] );
+
+        return $message;
     }
 }
